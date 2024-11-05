f79f0082fc7323a87b491fac20eb0b005350e83d
Evgenia Bullis <bulish2015@yandex.ru>
2024-11-05 20:27:26 +0300

    initial commit

---
+++ src/main/java/com/chess/jnd/ChessJndApplication.java
@@ -0,0 +1,13 @@
+package com.chess.jnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessJndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessJndApplication.class, args);
+	}
+
+}
