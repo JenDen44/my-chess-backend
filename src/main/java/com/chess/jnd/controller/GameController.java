67ceb6fc587a59d11bd578c149946244a8419d59
Evgenia Bullis <bulish2015@yandex.ru>
2024-12-06 18:28:26 +0300

    add swagger.

--- src/main/java/com/chess/jnd/controller/GameController.java
+++ src/main/java/com/chess/jnd/controller/GameController.java
@@ -6,6 +6,9 @@ import com.chess.jnd.entity.GameResponse;
 import com.chess.jnd.entity.MoveRequest;
 import com.chess.jnd.service.GameCommonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Tag(name = "Game controller")
 public class GameController {
 
     private final GameCommonService gameService;
@@ -22,22 +26,92 @@ public class GameController {
         this.gameService = gameService;
     }
 
+    @Operation(
+            description = "Endpoint to create new game",
+            summary = "If you need to start new game, please use this endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+
+                    @ApiResponse(
+                            description = "Validation error",
+                            responseCode = "422"
+                    )
+            }
+    )
     @PostMapping("/start")
     public ResponseEntity<GameResponse> createNewGame(@RequestBody CreateGameRequest gameRequest) throws JsonProcessingException {
        return ResponseEntity.ok(gameService.createGame(gameRequest));
     }
 
+    @Operation(
+            description = "Endpoint to get current game",
+            summary = "If you need to to get current game, please use this endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+
+                    @ApiResponse(
+                            description = "Validation error",
+                            responseCode = "422"
+                    ),
+
+                    @ApiResponse(
+                            description = "Not found",
+                            responseCode = "404"
+                    )
+            }
+    )
     @GetMapping("/game")
     public ResponseEntity<GameResponse> currentGame(HttpServletRequest request) throws JsonProcessingException {
         return ResponseEntity.ok(gameService.getCurrentGame(request));
     }
 
+    @Operation(
+            description = "Endpoint to perform move on board",
+            summary = "If you need to perform move on board, please use this endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+
+                    @ApiResponse(
+                            description = "Validation error",
+                            responseCode = "422"
+                    )
+            }
+    )
     @PostMapping("/game/move")
     public ResponseEntity<GameInfoResponse> move(HttpServletRequest request,
                                                  @RequestBody MoveRequest moveRequest) throws JsonProcessingException {
         return ResponseEntity.ok(gameService.move(request, moveRequest));
     }
 
+    @Operation(
+            description = "Endpoint to give up",
+            summary = "If you need give up, please use this endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+
+                    @ApiResponse(
+                            description = "Validation error",
+                            responseCode = "422"
+                    ),
+
+                    @ApiResponse(
+                            description = "Not found",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PostMapping("/game/give-up")
     public ResponseEntity<GameInfoResponse> giveUp(HttpServletRequest request) throws JsonProcessingException {
         return  ResponseEntity.ok(gameService.giveUp(request));
--- src/main/java/com/chess/jnd/entity/GameResponse.java
+++ src/main/java/com/chess/jnd/entity/GameResponse.java
@@ -1,6 +1,7 @@
 package com.chess.jnd.entity;
 
 import com.chess.jnd.entity.figures.ShortFigureName;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,16 +12,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Game Response Model Information")
 public class GameResponse {
+
+    @Schema(description = "Token for white player", example = "set of random symbols")
     private String tokenForWhitePlayer;
 
+    @Schema(description = "Token for black player", example = "set of random symbols")
     private String tokenForBlackPlayer;
 
+    @Schema(description = "Game info (status, details)", example = "status: finished, in_process / result: white / black / draw")
     private GameInfo info;
 
+    @Schema(description = "Board matrix contains figures", example = "[['k'],['q']]")
     private ShortFigureName[][] board;
 
+    @Schema(description = "Active color Enum type", example = "white / black")
     private Color active;
 
+    @Schema(description = "Current color Enum type", example = "white / black")
     private Color currentColor;
 }
