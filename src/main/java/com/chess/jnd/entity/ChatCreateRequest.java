572e0ca84e81b6662f866e15993bdbbfda0ea728
Evgesha-thunder <72350114+Evgesha-thunder@users.noreply.github.com>
2025-01-25 17:01:28 +0300

    create client call.

---
+++ src/main/java/com/chess/jnd/entity/ChatCreateRequest.java
@@ -0,0 +1,15 @@
+package com.chess.jnd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatCreateRequest {
+
+    private List<String> tokens;
+}
---
+++ src/main/java/com/chess/jnd/entity/ChatResponse.java
@@ -0,0 +1,15 @@
+package com.chess.jnd.entity;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatResponse {
+    private Integer id;
+
+    List<String> tokens;
+}
